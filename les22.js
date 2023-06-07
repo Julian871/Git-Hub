993326f50026082352b567ba9b0d034e7d92f762
Julian Medvedev <vindiesel.1999@mail.ru>
2023-06-07 21:34:27 +0300

    22

---
+++ les22.js
@@ -0,0 +1,11 @@
+const myDog = {
+    // Only change code below this line
+    name: 'Chapi',
+    legs: 15,
+    tails: 10,
+    friends: ['Chop', 'Cop'],
+    
+  
+  
+    // Only change code above this line
+  };
