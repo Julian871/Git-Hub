f4d029ce048c93d4ce39827c924aad7c712d12c6
Julian Medvedev <vindiesel.1999@mail.ru>
2023-06-09 12:23:50 +0300

    30

---
+++ les30.js
@@ -0,0 +1,15 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": ["CD", "8T", "LP"],
+      "gold": true,
+    },
+    {
+      "artist": "Julian Medvedev",
+      "title": "Samsara",
+      "release_year": 2022,
+      "formats": ["CD", "8T", "LP"],
+    }
+  ];
